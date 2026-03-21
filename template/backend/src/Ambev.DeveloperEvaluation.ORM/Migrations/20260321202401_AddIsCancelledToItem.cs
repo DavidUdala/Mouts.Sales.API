36842b29b303760da7af27713ae52891be82c991
David Udala <david_udala@hotmail.com>
2026-03-21 19:24:31 -0300

    chore: new migrations IsCancelled for SaleItem and product data

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20260321202401_AddIsCancelledToItem.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCancelledToItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCancelled",
+                table: "SaleItem",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsCancelled",
+                table: "SaleItem",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+        }
+    }
+}
